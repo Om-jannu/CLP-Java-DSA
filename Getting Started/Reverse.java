75263a428314cb790dff6c8c6a709f17c1562596
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2021-11-14 19:51:43 +0530

    Update Reverse.java

--- Getting Started/Reverse.java
+++ Getting Started/Reverse.java
@@ -8,5 +8,15 @@ import java.util.*;
 public class Reverse {
     public static void main(String[] args) {
        // write your code here   
+          Scanner sc = new Scanner(System.in);
+        System.out.println("Enter series of numbers : ");
+        int n = sc.nextInt();
+        int rem = 0;
+
+        while(n>0){
+                rem = rem*10 + n%10;
+                n = n/10;
+        }
+        System.out.println(rem);
     }
 }
