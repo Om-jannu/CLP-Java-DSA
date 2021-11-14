7a5c35b5da644f3d9525934fd87fc6f669d4998d
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2021-11-14 19:31:05 +0530

    Update CountDigits.java

--- Getting Started/CountDigits.java
+++ Getting Started/CountDigits.java
@@ -8,6 +8,15 @@ import java.util.*;
   public class CountDigits{
   
   public static void main(String[] args) {
-    // write your code here  
+    // write your code here 
+       Scanner sc = new Scanner(System.in);
+      int count=0;
+      System.out.println("Enter series of numbers : ");
+      int n = sc.nextInt();
+      while(n!=0){
+          n=n/10;
+          count++;
+      }
+      System.out.println("number of digits in "+n +" is "+count);
    }
   }
